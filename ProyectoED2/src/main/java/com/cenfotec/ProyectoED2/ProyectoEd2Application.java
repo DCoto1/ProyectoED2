39808a8a106cebf74302528a72a8a893c75b5b64
David <drodriguezc1@ucenfotec.ac.cr>
2022-11-08 21:11:15 -0600

    Commit inicial del repositorio

---
+++ ProyectoED2/src/main/java/com/cenfotec/ProyectoED2/ProyectoEd2Application.java
@@ -0,0 +1,13 @@
+package com.cenfotec.ProyectoED2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoEd2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoEd2Application.class, args);
+	}
+
+}
---
+++ ProyectoED2/src/test/java/com/cenfotec/ProyectoED2/ProyectoEd2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cenfotec.ProyectoED2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoEd2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
