824aac1fd64b6abe3efdcdb5c1e1d20116388fb6
David <drodriguezc1@ucenfotec.ac.cr>
2022-11-09 21:55:24 -0600

    09/11/2022. Implementación de zoom 'onclick' al darle click a un marcador

--- ProyectoED2_UI/js/scripts.js
+++ ProyectoED2_UI/js/scripts.js
@@ -3,7 +3,7 @@ let map;
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 9.748917, lng: -83.753428},
-    zoom: 10,
+    zoom: 12,
   });
   let response;
   fetch("http://localhost:8080/api/proyecto/getLugares",{
@@ -21,11 +21,15 @@ function initMap() {
           json => {
             var select = document.getElementById("select");
             for (var i = 0; i < json.length; i++){
-              new google.maps.Marker({
+              var marker = new google.maps.Marker({
                 position: { lat: json[i].latitud, lng: json[i].longitud },
                 map,
                 title: json[i].nombre,
               });
+              google.maps.event.addListener(marker, 'click', function() {
+                map.panTo(this.getPosition());
+                map.setZoom(14);
+              }); 
               select.insertAdjacentHTML("beforeend", "<option value='"+ json[i].id +"'>"+ json[i].nombre +"</option>");
             }
 
