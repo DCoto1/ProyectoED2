e31fd6571835252cde7f77587aa8ea8f8c6bf748
David <drodriguezc1@ucenfotec.ac.cr>
2022-11-11 11:08:22 -0600

    Funcionalidad de enlaze de las rutas añadido

--- ProyectoED2_UI/js/scripts.js
+++ ProyectoED2_UI/js/scripts.js
@@ -1,6 +1,7 @@
 let map;
 
 function initMap() {
+  const nodosRuta = [];
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 9.748917, lng: -83.753428},
     zoom: 10,
@@ -30,7 +31,20 @@ function initMap() {
                 map.panTo(this.getPosition());
               }); 
               select.insertAdjacentHTML("beforeend", "<option value='"+ json[i].id +"'>"+ json[i].nombre +"</option>");
+              nodosRuta.push(new google.maps.LatLng(
+                json[i].latitud,
+                json[i].longitud
+              ));
             }
+            const rutaEnlazada = new google.maps.Polyline({
+              path: nodosRuta,
+              geodesic: true,
+              strokeColor: "#00000",
+              strokeOpacity: 1.0,
+              strokeWeight: 2,
+            });
+      
+            rutaEnlazada.setMap(map);
 
           }
       )
